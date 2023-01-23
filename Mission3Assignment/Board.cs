744a551a3d2720172e04695afd6ed572b8e7bf03
corbanpro <corbanpro@gmail.com>
2023-01-23 15:56:51 -0700

    print board

--- Mission3Assignment/Board.cs
+++ Mission3Assignment/Board.cs
@@ -6,14 +6,31 @@ namespace Mission3Assignment
 {
     class Board
     {
-        public void PrintBoard(bool[,] board)
+        public void PrintBoard(string[] board)
         {
+            string fillerLine = "     |     |     ";
+            string bottomLine = "_____|_____|_____";
+
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = "asdf";
+            }
+
+            Console.WriteLine(fillerLine);
+            Console.WriteLine("__" + board[0] + "__|__" + board[1] + "__|__" + board[2] + "__");
+            Console.WriteLine(bottomLine);
+            Console.WriteLine(fillerLine);
+            Console.WriteLine("__" + board[3] + "__|__" + board[4] + "__|__" + board[5] + "__");
+            Console.WriteLine(bottomLine);
+            Console.WriteLine(fillerLine);
+            Console.WriteLine("__" + board[6] + "__|__" + board[7] + "__|__" + board[8] + "__");
+            Console.WriteLine(fillerLine);
 
         }
 
-        public string Winner(bool[,] board)
+        public string Winner(string[] board)
         {
-            string winner = '';
+            string winner = "";
 
             return winner;
         }
