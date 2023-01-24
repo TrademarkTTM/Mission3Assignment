5cbae19d3ae93981c1beb9cd95d9b5d7a736be11
corbanpro <corbanpro@gmail.com>
2023-01-24 14:06:48 -0700

    should be working now lol

--- Mission3Assignment/Board.cs
+++ Mission3Assignment/Board.cs
@@ -27,29 +27,29 @@ namespace Mission3Assignment
         {
             string winner = "";
 
-            if (board[0] == board[1]) and (board[1] == board[2]) {
-                winner = (string)board[0]
+            if (board[0] == board[1]) && (board[1] == board[2]) {
+                winner = (string)board[0];
             }
-            if (board[3] == board[4]) and (board[4] == board[5]) {
-                winner = (string)board[3]
+            if (board[3] == board[4]) && (board[4] == board[5]) {
+                winner = (string)board[3];
             }
-            if (board[6] == board[7]) and (board[7] == board[8]) {
-                winner = (string)board[6]
+            if (board[6] == board[7]) && (board[7] == board[8]) {
+                winner = (string)board[6];
             }
-            if (board[0] == board[3]) and (board[3] == board[6]) {
-                winner = (string)board[0]
+            if (board[0] == board[3]) && (board[3] == board[6]) {
+                winner = (string)board[0];
             }
-            if (board[1] == board[4]) and (board[4] == board[7]) {
-                winner = (string)board[1]
+            if (board[1] == board[4]) && (board[4] == board[7]) {
+                winner = (string)board[1];
             }
-            if (board[2] == board[5]) and (board[5] == board[8]) {
-                winner = (string)board[2]
+            if (board[2] == board[5]) && (board[5] == board[8]) {
+                winner = (string)board[2];
             }
-            if (board[0] == board[4]) and (board[4] == board[8]) {
-                winner = (string)board[0]
+            if (board[0] == board[4]) && (board[4] == board[8]) {
+                winner = (string)board[0];
             }
-            if (board[2] == board[4]) and (board[4] == board[6]) {
-                winner = (string)board[2]
+            if (board[2] == board[4]) && (board[4] == board[6]) {
+                winner = (string)board[2];
             }
 
             return winner;
