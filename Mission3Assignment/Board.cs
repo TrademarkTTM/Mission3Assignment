c5b66e7df4f9c16e53f69b60894a27c5956ec492
corbanpro <corbanpro@gmail.com>
2023-01-24 14:08:45 -0700

    should work now for real

--- Mission3Assignment/Board.cs
+++ Mission3Assignment/Board.cs
@@ -27,28 +27,28 @@ namespace Mission3Assignment
         {
             string winner = "";
 
-            if (board[0] == board[1]) && (board[1] == board[2]) {
+            if ((board[0] == board[1]) && (board[1] == board[2])) {
                 winner = (string)board[0];
             }
-            if (board[3] == board[4]) && (board[4] == board[5]) {
+            if ((board[3] == board[4]) && (board[4] == board[5])) {
                 winner = (string)board[3];
             }
-            if (board[6] == board[7]) && (board[7] == board[8]) {
+            if ((board[6] == board[7]) && (board[7] == board[8])) {
                 winner = (string)board[6];
             }
-            if (board[0] == board[3]) && (board[3] == board[6]) {
+            if ((board[0] == board[3]) && (board[3] == board[6])) {
                 winner = (string)board[0];
             }
-            if (board[1] == board[4]) && (board[4] == board[7]) {
+            if ((board[1] == board[4]) && (board[4] == board[7])) {
                 winner = (string)board[1];
             }
-            if (board[2] == board[5]) && (board[5] == board[8]) {
+            if ((board[2] == board[5]) && (board[5] == board[8])) {
                 winner = (string)board[2];
             }
-            if (board[0] == board[4]) && (board[4] == board[8]) {
+            if ((board[0] == board[4]) && (board[4] == board[8])) {
                 winner = (string)board[0];
             }
-            if (board[2] == board[4]) && (board[4] == board[6]) {
+            if ((board[2] == board[4]) && (board[4] == board[6])) {
                 winner = (string)board[2];
             }
 
